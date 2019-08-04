3372104b564adb92f6f13e0ba89334edd9da7ac7
Elijah Hall <ehall851@gmail.com>
2019-08-04 10:15:41 -0600

    created todo objects with ids and booleans to render in dom

---
+++ react-crash-todo/src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Todoitem from './Todoitem';
+
+class Todos extends Component {
+  render() {
+      console.log(this.props.todos)
+    return this.props.todos.map((todo) => (
+        <Todoitem key={todo.id} todo= {todo} />
+    ))
+  }
+  
+}
+
+export default Todos;
