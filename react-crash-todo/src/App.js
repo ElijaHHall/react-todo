7e7231a7006050f551469d4857e0e4d250f6aae2
Elijah Hall <ehall851@gmail.com>
2019-08-05 08:26:54 -0600

    changing the state of each todo when checked and can now delete from dom

--- react-crash-todo/src/App.js
+++ react-crash-todo/src/App.js
@@ -22,16 +22,27 @@ state = {
     },
   ] 
 }
-
+// Toggle Complete
 markComplete = (id) => {
-  console.log(id)
+  this.setState({ todos: this.state.todos.map(todo => {
+    if (todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo;
+  })})
 }
 
+//Delete Todo
+delTodo = (id) => {
+  this.setState({ todos: [...this.state.todos.filter(todo => todo.id
+    !== id)] })
+}
   render() {
     return (
     <div className="App">
       <Todos todos={this.state.todos} 
-      markComplete = {this.markComplete}/>
+      markComplete = {this.markComplete} 
+      delTodo={this.delTodo} />
     </div>
   );
   }
--- react-crash-todo/src/components/Todoitem.js
+++ react-crash-todo/src/components/Todoitem.js
@@ -21,17 +21,28 @@ render() {
     return (
         <div style={this.getStyle()}>
             <p>
-            <input type="checkbox" onChange={this.props.markComplete.bind
-            (this, id)} /> {' '}
+            <input type="checkbox" onChange={this.props.markComplete.bind (this, id)} /> {' '}
             {title}
+            <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
             </p>
         </div>
     )
 }
 }
 
+//Prop Types
 Todoitem.propTypes= {
     todo: PropTypes.object.isRequired
+  }
+
+  const  btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
   }
 
 
